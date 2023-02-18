e2a9e7731d0a4b0fce47e7403e2a2ee285197a7a
Emmanuel Anebi <anebiemmanuel@gmail.com>
2023-02-18 15:20:24 +0100

    :white_check_mark: refactor: use correct base url for axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:8000/api/v1',
-    baseURL: 'http://localhost:8000/api/v1/',
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
         'Content-type': 'application/json',
     },
