257f17c560c93d3ec4501f3733045e40ab60247c
Emmanuel Anebi <anebiemmanuel@gmail.com>
2023-03-18 20:40:20 +0100

    add: authentication checks and permissions to protected routes

--- src/components/libs/hoc/dashboard/index.tsx
+++ src/components/libs/hoc/dashboard/index.tsx
@@ -1,17 +1,57 @@
-import Sidebar from 'components/organisms/o-dashboard-sidebar';
+// import React, { useMemo } from 'react';
+// // import { useNavigate } from 'react-router-dom';
+// import { checkAuthToken } from 'utils/auth-cookies';
+
+// function WithDashboard(WrappedComponent: any) {
+//     // const navigate = useNavigate();
+//     const hasAuthorization = checkAuthToken();
+
+//     const WithDashboardHOCComponent = useMemo(() => {
+//         // if (hasAuthorization) {
+//         return function WithDashboardComponent({ ...props }) {
+//             return <WrappedComponent {...props} />;
+//         };
+//         // }
+//         // navigate('/login');
+//         // return null;
+//     }, [WrappedComponent, hasAuthorization]);
+
+//     return WithDashboardHOCComponent;
+// }
+
+// export default WithDashboard;
+
 import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+import { checkAuthToken } from 'utils/auth-cookies';
 
 function WithDashboard(WrappedComponent: any) {
-    return function WithDashboardComponent({ ...props }) {
-        return (
-            <>
-                <Sidebar />
-                <div className="absolute h-full w-[calc(100%-16.5rem)] left-[16.5rem]">
-                    <WrappedComponent {...props} />
-                </div>
-            </>
-        );
-    };
+    // const navigate = useNavigate();
+    const hasAuthorization = checkAuthToken();
+    if (hasAuthorization) {
+        // eslint-disable-next-line react/no-unstable-nested-components
+        return function WithDashboardComponent({ ...props }) {
+            return <WrappedComponent {...props} />;
+        };
+    }
+    window.location.replace(`${window.location.origin}/login`);
 }
 
 export default WithDashboard;
+
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { checkAuthToken } from 'utils/auth-cookies';
+
+// function WithDashboard(WrappedComponent: any) {
+//     const hasAuthorization = checkAuthToken();
+//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//     const navigate = useNavigate();
+//     console.log(hasAuthorization);
+//     // eslint-disable-next-line react/no-unstable-nested-components
+//     return function WithDashboardComponent({ ...props }) {
+//         return <WrappedComponent {...props} />;
+//     };
+// }
+
+// export default WithDashboard;
--- src/components/libs/layout/dashboard/index.tsx
+++ src/components/libs/layout/dashboard/index.tsx
@@ -2,20 +2,13 @@ import DashboardTopNavigation from 'components/molecules/m-dashboard-top-navigat
 import Sidebar from 'components/organisms/o-dashboard-sidebar';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
-// interface IToggleOpenSidebarProps {
-//     openSidebar: boolean
-//     toggleOpenSidebar: () => void
-// }
+import { checkAuthToken } from 'utils/auth-cookies';
 
 function DashboardLayout() {
-    // const { width }: Size = useWindowSize();
-    // const [openSidebar, toggleOpenSidebar]: [boolean, () => void] = useToggle();
-    // const [shiftDashboardContent, toggleShiftDashboardContent] = useToggle();
-    // const [openDrawer, toggleOpenDrawer] = useToggle();
-    // const SHOW = true;
-    // if (width) {
-    // if (width > 768) {
+    const hasAuthorization = checkAuthToken();
+    if (!hasAuthorization) {
+        window.location.replace(`${window.location.origin}/login`);
+    }
     return (
         <>
             <Sidebar />
@@ -29,43 +22,78 @@ function DashboardLayout() {
             </div>
         </>
     );
-    // }
-
-    // if (width > 480 && width <= 768) {
-    //     return (
-    //         <>
-    //             <Sidebar
-    //                 openDrawer={openDrawer}
-    //                 toggleOpenDrawer={toggleOpenDrawer}
-    //                 showMenuIcon={SHOW}
-    //                 toggleShiftDashboardContent={toggleShiftDashboardContent}
-    //             />
-    //             <div
-    //                 className={`dashboard-content transition-all ease-in-out duration-450 ${
-    //                     shiftDashboardContent ? 'translate-x-[9rem]' : ''
-    //                 }`}
-    //             >
-    //                 <div className="flex-flex-column p-2 space-y-2">
-    //                     <MainTopNavigation />
-    //                     <div>{children}</div>
-    //                 </div>
-    //             </div>
-    //         </>
-    //     );
-    // }
-
-    // if (width < 480) {
-    //     return (
-    //         <>
-    //             <SidebarSlideOver openSidebar={openSidebar} toggleOpenSidebar={toggleOpenSidebar}>
-    //                 <Sidebar openDrawer={SHOW} showMenuIcon={!SHOW} />
-    //             </SidebarSlideOver>
-    //             <MobileTopNavigation toggleOpenSidebar={toggleOpenSidebar} />
-    //             <div className="p-4">{children}</div>
-    //         </>
-    //     );
-    // }
-    // }
 }
 
 export default DashboardLayout;
+
+// import WithDashboard from 'components/libs/hoc/dashboard';
+// import DashboardTopNavigation from 'components/molecules/m-dashboard-top-navigation';
+// import Sidebar from 'components/organisms/o-dashboard-sidebar';
+// import React from 'react';
+// import { Outlet } from 'react-router-dom';
+// import { checkAuthToken } from 'utils/auth-cookies';
+
+// interface IToggleOpenSidebarProps {
+//     openSidebar: boolean
+//     toggleOpenSidebar: () => void
+// }
+
+// function DashboardLayout() {
+// const { width }: Size = useWindowSize();
+// const [openSidebar, toggleOpenSidebar]: [boolean, () => void] = useToggle();
+// const [shiftDashboardContent, toggleShiftDashboardContent] = useToggle();
+// const [openDrawer, toggleOpenDrawer] = useToggle();
+// const SHOW = true;
+// return (
+//     <>
+//         <Sidebar />
+//         <div className="dashboard-content">
+//             <div className="flex-flex-column p-2 space-y-2">
+//                 <DashboardTopNavigation />
+//                 <div>
+//                     <Outlet />
+//                 </div>
+//             </div>
+//         </div>
+//     </>
+// );
+// }
+
+// if (width > 480 && width <= 768) {
+//     return (
+//         <>
+//             <Sidebar
+//                 openDrawer={openDrawer}
+//                 toggleOpenDrawer={toggleOpenDrawer}
+//                 showMenuIcon={SHOW}
+//                 toggleShiftDashboardContent={toggleShiftDashboardContent}
+//             />
+//             <div
+//                 className={`dashboard-content transition-all ease-in-out duration-450 ${
+//                     shiftDashboardContent ? 'translate-x-[9rem]' : ''
+//                 }`}
+//             >
+//                 <div className="flex-flex-column p-2 space-y-2">
+//                     <MainTopNavigation />
+//                     <div>{children}</div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
+
+// if (width < 480) {
+//     return (
+//         <>
+//             <SidebarSlideOver openSidebar={openSidebar} toggleOpenSidebar={toggleOpenSidebar}>
+//                 <Sidebar openDrawer={SHOW} showMenuIcon={!SHOW} />
+//             </SidebarSlideOver>
+//             <MobileTopNavigation toggleOpenSidebar={toggleOpenSidebar} />
+//             <div className="p-4">{children}</div>
+//         </>
+//     );
+// }
+// }
+// }
+
+// export default DashboardLayout;
