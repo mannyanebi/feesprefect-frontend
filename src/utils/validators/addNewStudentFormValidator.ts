13942b3f8800b78b8d3e9a65f2aaf7087c7d753d
Emmanuel Anebi <anebiemmanuel@gmail.com>
2023-05-22 09:15:42 +0100

    fix: validation for date of birth field

--- src/utils/validators/addNewStudentFormValidator.ts
+++ src/utils/validators/addNewStudentFormValidator.ts
@@ -3,6 +3,7 @@ import * as yup from 'yup';
 const AddNewStudentSchema = yup.object().shape({
     'Academic Class': yup.string().required().typeError('Academic Class is required'),
     'Student Name': yup.string().required().min(2).typeError('Student Name is Required'),
+    'Date of Birth': yup.string().required().typeError('Date of Birth is Required'),
 });
 
 export default AddNewStudentSchema;
