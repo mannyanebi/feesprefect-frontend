38c272a551031c4055718151fa14a9775cce0f28
Emmanuel Anebi <anebiemmanuel@gmail.com>
2023-06-01 13:43:01 +0100

    fix: school statistics to show amount recorded in the app

--- src/components/atoms/a-number-of-students-stat-card/index.tsx
+++ src/components/atoms/a-number-of-students-stat-card/index.tsx
@@ -6,7 +6,7 @@ function NumberOfStudentsStat({ numberOfStudents }: { numberOfStudents: number }
         <div className="h-[12rem] w-[12rem] bg-white shadow-md p-4 flex flex-col items-center justify-center rounded-md space-y-4">
             <img src={StudentStatSvg} className="w-[3rem]" alt="student statistics" />
             <span className="font-bold text-[1.5rem]">{numberOfStudents}</span>
-            <span className="text-gray-600 text-center text-sm">Total number of Students</span>
+            <span className="text-gray-600 text-center text-sm">Total number active of Students</span>
         </div>
     );
 }
--- src/components/atoms/a-total-amount-of-school-fees-stat/index.tsx
+++ src/components/atoms/a-total-amount-of-school-fees-stat/index.tsx
@@ -5,7 +5,7 @@ function TotalAmountOfSchoolFeesStat({ totalAmountOfSchoolFees }: { totalAmountO
     return (
         <div className="h-[12rem] w-[12rem] bg-white shadow-md p-4 flex flex-col items-center justify-center rounded-md space-y-4">
             <img src={MoneyStatSvg} className="w-[3rem]" alt="student statistics" />
-            <span className="font-bold text-[1.5rem]">N {totalAmountOfSchoolFees}</span>
+            <span className="font-bold text-[1.5rem]">N {totalAmountOfSchoolFees.toLocaleString('en-US')}</span>
             <span className="text-gray-600 text-center text-sm">Total amount of school fees Payments</span>
         </div>
     );
